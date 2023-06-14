2aa3fbbf2cd58133fdf00e4524457d9d7949b324
GreenCatInForest <event2016.2017@gmail.com>
2023-06-14 20:42:12 +0100

    change font size name in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 export const Header = () => {
-  const h1ClassNames = "display-1 p-5";
+  const h1ClassNames = "p-5 text-center";
+  const nameClassNames = {
+    fontWeight: "bold",
+    fontSize: "8rem",
+  };
 
   return (
     <div>
-      <h1 className={h1ClassNames}>Hello. I'm YANA.</h1>
+      <h1 className={h1ClassNames}>
+        Hello. I'm <div style={nameClassNames}>YANA.</div>
+      </h1>
       <hr />
       <h2>Here is my portfolio</h2>
     </div>
