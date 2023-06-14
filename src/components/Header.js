c7a85f46141ba27cddbe04bcadef20ea66c3c08d
GreenCatInForest <event2016.2017@gmail.com>
2023-06-14 19:37:50 +0100

    add header bootstrap style multiple classNames

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 export const Header = () => {
+  const h1ClassNames = "display-1 p-5";
+
   return (
     <div>
-      <h1>Hello. I'm YANA.</h1>
+      <h1 className={h1ClassNames}>Hello. I'm YANA.</h1>
       <hr />
       <h2>Here is my portfolio</h2>
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,32 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Stack from "react-bootstrap/Stack";
 
 export const Navigation = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">somethingFancy</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home"> Projects</Nav.Link>
-            <Nav.Link href="#link">About me</Nav.Link>
-            <Nav.Link href="#link">Contact me</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Stack gap={2} className="col-md-5 mx-auto">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">somethingFancy</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" bg="dark" expand="lg">
+            <Nav className="me-auto">
+              <Nav.Link href="#home" gap={2} className="col-md-5 mx-auto">
+                {" "}
+                Projects
+              </Nav.Link>
+              <Nav.Link href="#link" gap={2} className="col-md-5 mx-auto">
+                About me
+              </Nav.Link>
+              <Nav.Link href="#link" gap={2} className="col-md-5 mx-auto">
+                Contact me
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Stack>
   );
 };
 
