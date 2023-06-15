d6ae71c2469bc2ca0c988f0e8ef4569230e64e00
GreenCatInForest <event2016.2017@gmail.com>
2023-06-15 21:17:26 +0100

    replace card design bootstrap

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,3 +1,5 @@
+import Card from "react-bootstrap/Card";
+
 export const ProjectCard = (props) => {
   const projectTitle = props.projectTitle;
   const projectSubTitle = props.projectSubTitle;
@@ -6,13 +8,19 @@ export const ProjectCard = (props) => {
   const projectGithub = props.projectGithub;
 
   return (
-    <div>
-      Here is a project card
-      <div className="projectTitle">{projectTitle}</div>
-      <div className="projectSubTitle">{projectSubTitle}</div>
-      <div className="projectImg">{projectImg}</div>
-      <div className="projectLink">{projectLink}</div>
-      <div className="projectGithub ">{projectGithub}</div>
-    </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>{projectTitle}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          {projectSubTitle}
+        </Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
   );
 };
